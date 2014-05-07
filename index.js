b0388e0f4370e9a0ca0ef2f364b270c1b46ff5d6
liupeng <dp90219@gmail.com>
2014-05-07 14:13:09 +0800

    The Listen Method

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-var connect = require('connect');
+var http = require('http');
 
 var app;
 
 module.exports = function() {
-  app = connect();
+  app = function(req, res) {
+    res.statusCode = 404;
+    res.end();
+  };
+
+  app.listen = function() {
+    var server = http.createServer(this);
+    return server.listen.apply(server, arguments);
+  };
+
   return app;
 }
