f7403e9fd644a4bb0f9b8197b9329e7b5a357e15
liupeng <dp90219@gmail.com>
2014-05-08 20:23:20 +0800

    Implement Middleware Stack

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 var http = require('http');
 
+
 module.exports = function() {
+  var index = 0;
   var app = function(req, res, next) {
     app.handle(req, res, next);
   };
 
   app.handle = function(req, res, next) {
     var stack = this.stack;
-    res.statusCode = 404;
-    res.end();
+    function next() {
+      var layer = stack[index++];
+      if (!layer) {
+        res.statusCode = 404;
+        res.end();
+        return;
+      }
+      layer.handle(req, res, next);
+    }
+
+    next();
+
   };
 
   app.use = function(fn) {
-    this.stack.push(fn);
+    this.stack.push({handle: fn});
+    return this;
   }
 
   app.listen = function() {
